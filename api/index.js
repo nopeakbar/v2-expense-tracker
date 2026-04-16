e818ef4f250047117acb0e09bbbb632e6a80b69e
nopeakbar <nopeakbar@gmail.com>
2026-04-16 22:00:57 +0700

    biar bisa terima gamabr

--- api/index.js
+++ api/index.js
@@ -12,7 +12,8 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
 const geminiApiKey = process.env.GEMINI_API_KEY;
